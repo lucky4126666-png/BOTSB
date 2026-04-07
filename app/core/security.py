6c94461ed16678fe24a3385c72a8a0af159f095c
Nguyen Long <lucky4126666@gmail.com>
2026-04-07 08:03:22 +0700

    Delete app directory

--- app/core/security.py
+++
@@ -1,6 +0,0 @@
-from fastapi import HTTPException, Request
-
-
-def require_admin_session(request: Request):
-    if not request.session.get("admin_ok"):
-        raise HTTPException(status_code=401, detail="Unauthorized")
